11d67f3b64924927e34522b4c97c4e27d588a26e
jvoron <jvoron.ce888@gmail.com>
2020-09-11 13:59:07 +0300

    data mocked

--- src/app/task-view/task-view.component.ts
+++ src/app/task-view/task-view.component.ts
@@ -13,3 +13,38 @@ export class TaskViewComponent implements OnInit {
   }
 
 }
+
+export const TASKS: any ={
+  
+  {date: '2020-09-11',
+  quantity: 5,
+  price: 6,
+  eventTypeName: 'Lunch',
+  isExpenseType: false,
+  isHoursEventType: true,
+  isAdditionalHoursEventType: false,
+  isWorkHour: false,
+  isApproved: false,
+  isRejected: false,
+  tasksCount: 2,
+  firstTaskStart: '2020-09-11 08:00',
+  lastTaskEnd: '2020-09-11 17:00'
+  },
+  {date: '2020-09-12',
+  quantity: 5,
+  price: 6,
+  eventTypeName: 'Lunch',
+  isExpenseType: false,
+  isHoursEventType: true,
+  isAdditionalHoursEventType: false,
+  isWorkHour: false,
+  isApproved: false,
+  isRejected: false,
+  tasksCount: 2,
+  firstTaskStart: '2020-09-12 08:00',
+  lastTaskEnd: '2020-09-12 17:00'
+  }
+
+
+
+}
